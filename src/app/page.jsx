71b618854fc682ef33c44a40f19b6b281a0e5c08
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2023-06-21 10:45:44 +0200

    added social media icons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero/Hero";
+import SocialMedia from "@/components/socialMedia/SocialMediaIcons";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <SocialMedia />
       <div>Home</div>
     </main>
   );
---
+++ src/components/socialMedia/SocialMediaIcons.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+import s from "./socialmediaicons.module.scss";
+
+const SocialMedia = () => {
+  return (
+    <div className={`container`}>
+      <div className="row">
+        <div className={`col-6 col-md-12 `}>
+          <div className={s.images}>
+            <Image src="/google.png" width={112} height={36} alt="google" />
+            <Image src="/facebook.png" width={136} height={26} alt="facebook" />
+            <Image src="/youtube.png" width={126} height={28} alt="youtube" />
+            <Image
+              src="/pinterest.png"
+              width={140}
+              height={34}
+              alt="pinterest"
+            />{" "}
+            <Image src="/twitch.png" width={108} height={36} alt="twitch" />{" "}
+            <Image src="/webflow.png" width={134} height={34} alt="webflow" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
