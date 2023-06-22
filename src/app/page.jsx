fe825afc6daf0af0e19ba45696766bf826d45989
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2023-06-22 13:58:27 +0200

    fixing bugs for deployment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
-import GetStarted from "@/components/GetStarted/GetStarted";
-import Hero from "@/components/Hero/Hero";
-import Members from "@/components/Members/Members";
-import Questions from "@/components/Questions/Questions";
-import Results from "@/components/Results/Results";
-import SocialMedia from "@/components/SocialMedia/SocialMediaIcons";
+import GetStarted from "@/components/getStarted/GetStarted";
+import Hero from "@/components/hero/Hero";
+import Members from "@/components/members/Members";
+import Questions from "@/components/questions/Questions";
+import Results from "@/components/results/Results";
+import SocialMedia from "@/components/socialMedia/SocialMediaIcons";
 // import Image from "next/image";
 
 export default function Home() {
